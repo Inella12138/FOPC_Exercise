343bba9d74cf5bc3958c06b22678b223b30b257b
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-10 10:26:29 +0800

    Exercise G

--- Exercise_G/G_Q3.cs
+++ Exercise_G/G_Q3.cs
@@ -4,32 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise.Exercise_G
+namespace Exercise
 {
     internal class G_Q3
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter students' scores" +
-                "(seperate each subject by space, and seperate each student by enter, " +
-                "press enter when you finish): ");
-            int stu = 0;
-            while (true)
+            Console.Write("Please enter students' scores(12 students and 4 subjects): ");
+
+            int[,] mark = new int[12, 4];
+
+            for (int i = 0; i < 12; i++)
             {
                 string stringInput = Console.ReadLine();
-                if (stringInput == null) { break; }
-                stu++;
+                string[] user = stringInput.Split(' ');
 
-                char blk = ' ';
-                string[] user = stringInput.Split(blk);
-                int[] num = new int[user.Length];
-
-                for (int i = 0; i < user.Length; i++)
+                for (int j = 0; j < user.Length; j++)
                 {
-                    num[i] = int.Parse(user[i]);
+                    mark[i, j] = int.Parse(user[j]);
                 }
-            
             }
+
+            int[] total = new int[mark.GetLength(0)];
+            double[] avgStu = new double[mark.GetLength(0)];
+            for (int i = 0; i < mark.GetLength(0); i++)
+            {
+                for (int j = 0; j < mark.GetLength(1); j++)
+                {
+                    total[i] += mark[i, j];
+                }
+                avgStu[i] = total[i] / 4;
+            }
+
+            double[] avgSbj = new double[mark.GetLength(1)];
+            for (int j = 0; j < mark.GetLength(1); j++)
+            {
+                for (int i = 0; i < mark.GetLength(0); i++)
+                {
+                    avgSbj[j] += mark[i, j];
+                }
+                avgSbj[j] = avgSbj[j] / 12;
+            }
+
+            Console.WriteLine("Subject 1\tSubject 2\tSubject 3\tSubject 4\tTotal\tAvg\t");
+            for (int i = 0; i < mark.GetLength(0); i++)
+            {
+                for (int j = 0; j < mark.GetLength(1); j++)
+                {
+                    Console.Write($"{mark[i, j]}\t\t");
+                }
+                Console.WriteLine($"{total[i]}\t{avgStu[i]}");
+            }
+            Console.WriteLine("Average per subject");
+            for (int j = 0; j < mark.GetLength(1); j++)
+            {
+                Console.Write($"{avgSbj[j]}\t");
+            }
+            Console.WriteLine();
         }
     }
 }
